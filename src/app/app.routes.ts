9e973088909f9d09b183f30180dd8f17bc43da52
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-10-06 22:00:24 -0500

    feat: Implementar sidebar colapsable y corregir rutas para mantener sidebar en todas las vistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,33 @@
 import { Routes } from '@angular/router';
-import { Home } from './public/components/home/home';
+import { LayoutComponent } from './public/components/layout/layout';
 import { BOOKING_ROUTES } from './booking/presentation/views/booking.routes';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: Home
-  },
-  {
-    path: 'vehicle-status',
-    loadComponent: () => import('./booking/presentation/views/vehicle-unlock-status/vehicle-unlock-status').then(m => m.VehicleUnlockStatusComponent)
-  },
-  {
-    path: 'schedule-unlock',
-    loadComponent: () => import('./booking/presentation/views/schedule-unlock/schedule-unlock').then(m => m.ScheduleUnlockComponent)
-  },
-  {
-    path: 'booking',
-    children: BOOKING_ROUTES
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadComponent: () => import('./public/components/home/home').then(m => m.HomeComponent)
+      },
+      {
+        path: 'vehicle-status',
+        loadComponent: () => import('./booking/presentation/views/vehicle-unlock-status/vehicle-unlock-status').then(m => m.VehicleUnlockStatusComponent)
+      },
+      {
+        path: 'schedule-unlock',
+        loadComponent: () => import('./booking/presentation/views/schedule-unlock/schedule-unlock').then(m => m.ScheduleUnlockComponent)
+      },
+      {
+        path: 'booking',
+        children: BOOKING_ROUTES
+      }
+    ]
   }
 ];
--- src/app/public/components/sidebar/sidebar.ts
+++ src/app/public/components/sidebar/sidebar.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, MatIconModule, RouterModule],
+  imports: [CommonModule, MatIconModule, RouterModule, MatIconButton],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class SidebarComponent {
-
+  @Input() collapsed = false;
 }
