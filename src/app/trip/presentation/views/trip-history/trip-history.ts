f87627cf74660ffa11e0d5371e1266bd565ed805
Samuel <samuelbonifacio015@gmail.com>
2025-10-07 16:20:23 -0500

    feat: trip-history component setup

--- src/app/trip/presentation/views/trip-history/trip-history.ts
+++ src/app/trip/presentation/views/trip-history/trip-history.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
 
+interface Trip {
+  route: string;
+  date: string;
+  vehicle: string;
+  duration: string;
+  distance: string;
+  id: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-trip-history',
   imports: [],
   templateUrl: './trip-history.html',
   styleUrl: './trip-history.css'
 })
-export class TripHistory {
 
+export class TripHistory {
+  trips: Trip[] = [
+    {
+      route: '1068 Av. la Molina - 221 Av. Paz Soldán',
+      date: '12 Sept, 10:35 AM',
+      vehicle: 'Scooter Eléctrico',
+      duration: '55 min',
+      distance: '27.4 km',
+      id: '935d73bd-1ab5-437e-a678-e03c92c94134',
+      image: 'assets/scooter-red.png'
+    },
+    {
+      route: '513 Av. Universitaria - 2810 Av. de la Marina',
+      date: '17 Sept, 15:10 PM',
+      vehicle: 'Scooter Eléctrico',
+      duration: '32 min',
+      distance: '16.8 km',
+      id: 'daab8f54-6cd1-4198-a5a9-4dd3b2a14287',
+      image: 'assets/scooter-blue.png'
+    },
+    {
+      route: '1489 Av. Los Quechuas - 2390 Av. Primavera',
+      date: '28 Sept, 06:40 AM',
+      vehicle: 'Scooter Eléctrico',
+      duration: '1h45min',
+      distance: '67.2 km',
+      id: 'ef9c803c-2aca-4de3-a27b-f74da6bae7d7',
+      image: 'assets/scooter-green.png'
+    }
+  ];
+
+  printReceipt(trip: Trip) {
+    alert('Imprimiendo recibo del viaje: ' + trip.id);
+  }
+
+  viewDetails(trip: Trip) {
+    alert('Mostrando detalles del viaje: ' + trip.id);
+  }
+
+  seeMore() {
+    alert('Cargando más viajes...');
+  }
 }
