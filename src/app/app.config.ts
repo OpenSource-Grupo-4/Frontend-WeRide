33a6feff3dc9ee63ce4dbc54045cb776a9280a27
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-10-07 22:01:31 -0500

    feat: added trip-map

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideMapboxGL } from 'ngx-mapbox-gl';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -9,6 +9,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    provideMapboxGL({
+      accessToken: 'pk.eyJ1IjoiamhpbXlwb29sIiwiYSI6ImNtZGY4cjVoMDBheHcyaXEzaDV5a2g4eGIifQ.QYmwDCEn26DEW-8RbIG2jg'
+    })
   ]
 };
--- src/app/trip/presentation/views/trip-map/trip-map.ts
+++ src/app/trip/presentation/views/trip-map/trip-map.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit, signal} from '@angular/core';
+import { Map } from 'mapbox-gl';
+import { MapComponent, MarkerComponent} from 'ngx-mapbox-gl';
 
 @Component({
   selector: 'app-trip-map',
-  imports: [],
+  imports: [MapComponent, MarkerComponent],
   templateUrl: './trip-map.html',
   styleUrl: './trip-map.css'
 })
-export class TripMap {
+export class TripMap implements OnInit, OnDestroy {
+  userLocation = signal<[number, number] | null>(null)
 
+  private watchId?: number;
+
+  markers = [
+    {lng: -77.09451353035513, lat: -12.076631028447757},
+    {lng: -77.11978877469807, lat: -12.06576550096087},
+    {lng: -77.06801838656192, lat: -12.083350355463615},
+    {lng: -77.07102425059432, lat: -12.071962004217465},
+    {lng: -77.1073596422649, lat: -12.056230391783833}
+  ]
+
+  ngOnInit(): void {
+    if('geolocation' in navigator){
+      this.watchId = navigator.geolocation.watchPosition(
+        (pos) => {
+          const {longitude, latitude} = pos.coords;
+          this.userLocation.set([longitude, latitude]);
+        },
+        (error) => {
+          console.log('Error Obteniendo la ubicacion', error);
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 10000,
+          timeout: 20000,
+        }
+      );
+    } else {
+      alert('Tu navegador no soporta geolocalizacion')
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.watchId) {
+      navigator.geolocation.clearWatch(this.watchId);
+    }
+  }
 }
