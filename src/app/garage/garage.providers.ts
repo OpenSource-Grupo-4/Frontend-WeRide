341851700a0d9ca373003ab0932eea88ba3fb81d
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-10-10 16:36:08 -0500

    feat: create vehicle-details modal

--- src/app/garage/garage.providers.ts
+++ src/app/garage/garage.providers.ts
@@ -1,4 +1,5 @@
 import { Provider } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { VehicleRepository } from './application/repositories/vehicle.repository';
 import { VehicleRepositoryImpl } from './infrastructure/repositories/vehicle.repository.impl';
 import { GetVehiclesUseCase } from './application/use-cases/get-vehicles.usecase';
@@ -7,6 +8,7 @@ import { ToggleFavoriteUseCase } from './application/use-cases/toggle-favorite.u
 import { VehicleApiService } from './infrastructure/http/vehicle-api.service';
 
 export const GARAGE_PROVIDERS: Provider[] = [
+  MatDialog,
   VehicleApiService,
   GetVehiclesUseCase,
   FilterVehiclesUseCase,
