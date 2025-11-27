f5f045b8fd09ad9b104e82ea9800284102b96af3
Samuel <samuelbonifacio015@gmail.com>
2025-11-27 09:27:15 -0500

    feat: add user components

---
+++ src/app/user/presentation/views/user-section/user-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-section',
+  imports: [],
+  templateUrl: './user-section.html',
+  styleUrl: './user-section.css'
+})
+export class UserSection {
+
+}
