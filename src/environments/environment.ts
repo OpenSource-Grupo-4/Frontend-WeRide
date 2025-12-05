d620d11db9cc1246b3da595f4e4b5fa9f3703a04
Gabriel <U202418250@upc.edu.pe>
2025-12-04 20:44:34 -0500

    fix: update API URL for development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4200/api',  // Local development server
+  apiUrl: 'https://db-weride-4.onrender.com',  // Local development server
   endpoints: {
     users: '/users',
     vehicles: '/vehicles',
