ad20ad01d7a996b5b6263604cd525bea744ca27f
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-12-06 01:49:04 -0500

    chore: api url updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 //db.json deploy = https://db-weride-4.onrender.com
+//deploy previo = http://20.81.154.140:8080/
+//deploy actual = https://weride.duckdns.org/api/v1
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://weride.duckdns.org/api/v1',
   endpoints: {
     users: '/users',
     vehicles: '/garage/vehicles',
